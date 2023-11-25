730f6f82bb56aab4de2f6b6679b6601253a62ba5
Anton Trufanov <anton.t1984@gmail.com>
2023-11-25 20:50:07 +0300

    added middlewares: cors

--- internal/lib/httplib/constants.go
+++ internal/lib/httplib/constants.go
@@ -6,4 +6,8 @@ const (
 	ChunkNumHeaderKey            = "X-project-chunkNum"
 	SHA256FileChecksumHeaderKey  = "X-project-sha256FileChecksum"
 	SHA256ChunkChecksumHeaderKey = "X-project-sha256ChunkChecksum"
+
+	AccessControlAllowOrigin  = "Access-Control-Allow-Origin"
+	AccessControlAllowHeaders = "Access-Control-Allow-Headers"
+	AccessControlAllowMethod  = "Access-Control-Allow-Method"
 )
