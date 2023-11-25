ef15659ada0dc50dcda1efe46c10fbb7280e6bc1
Anton Trufanov <anton.t1984@gmail.com>
2023-11-25 23:25:59 +0300

    main: added app.Run()

--- cmd/service/main.go
+++ cmd/service/main.go
@@ -1,7 +1,27 @@
 package main
 
-import "fmt"
+import (
+	"log"
+
+	"github.com/antonT001/easy-storage-light/internal/config"
+	filemgr "github.com/antonT001/easy-storage-light/internal/file-mgr"
+	"github.com/antonT001/easy-storage-light/internal/rest"
+	"github.com/antonT001/easy-storage-light/internal/service"
+)
 
 func main() {
-	fmt.Println("hello world")
+	cfg, err := config.NewConfig("./config.yaml")
+	if err != nil {
+		log.Fatalf("failed to initialize configuration: %v", err)
+	}
+
+	fileMgr, err := filemgr.NewService(cfg.FileMgr)
+	if err != nil {
+		log.Fatalf("failed to initialize configuration fileMgr: %v", err)
+	}
+
+	svc := service.NewService(fileMgr)
+	app := rest.NewServer(cfg.Server, svc)
+
+	log.Fatal(app.Run())
 }
